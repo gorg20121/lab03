0565d82537462a4135d7f31e9bedd4f654435ad2
kapkin <kapkin228@gmail.ru>
2020-04-17 13:38:49 +0300

    code: Гистограма в цвете

--- main.cpp
+++ main.cpp
@@ -49,6 +49,8 @@ bins[bin]++;
 }
 return bins;
 }
+
+
 void show_histogram_text(const vector<size_t> &bins)
 {
 
@@ -91,6 +93,67 @@ cout << '*';
 cout << '\n';
 }
 }
+
+
+
+
+void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black")
+{
+cout << "<rect x='"<<x<<"' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
+}
+
+void svg_text(double left, double baseline, string text)
+{
+cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+;
+
+}
+void
+svg_begin(double width, double height) {
+cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+cout << "<svg ";
+cout << "width='" << width << "' ";
+cout << "height='" << height << "' ";
+cout << "viewBox='0 0 " << width << " " << height << "' ";
+cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void
+svg_end() {
+cout << "</svg>\n";
+}
+void
+show_histogram_svg(const vector<size_t>& bins) {
+const auto IMAGE_WIDTH = 400;
+const auto IMAGE_HEIGHT = 300;
+const auto TEXT_LEFT = 20;
+const auto TEXT_BASELINE = 20;
+const auto TEXT_WIDTH = 50;
+const auto BIN_HEIGHT = 30;
+const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH,IMAGE_HEIGHT);
+double top = 0;
+for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"darkviolet","#9400D3");
+    top += BIN_HEIGHT;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
 size_t number_count;
@@ -104,6 +167,6 @@ cin >> bin_count;
 double min, max;
 find_minmax(numbers, min, max);
 const auto bins = make_histogram(numbers, bin_count);
-show_histogram_text(bins);
+show_histogram_svg(bins);
 return 0;
 }
