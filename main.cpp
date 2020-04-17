911d138fa26303deee5951beef9f2d09c55fbc73
kapkin <kapkin228@gmail.ru>
2020-04-17 14:03:02 +0300

    code: добавил хедер

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -14,22 +15,6 @@ cin >> result[i];
 return result;
 }
 
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-min = numbers[0];
-max = numbers[0];
-for (double number : numbers)
-{
-if (number < min)
-{
-min = number;
-}
-if (number > max)
-{
-max = number;
-}
-}
-}
 
 vector <size_t> make_histogram(const vector<double> &numbers,size_t bin_count)
 {
