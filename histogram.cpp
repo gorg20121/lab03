13a743fe66cfb915630c80a5d9dbb4000ca07e81
kapkin <kapkin228@gmail.ru>
2020-04-17 15:14:08 +0300

    code: фактическое выполненение

---
+++ histogram.cpp
@@ -0,0 +1,25 @@
+#include "histogram.h"
+#include <vector>
+
+
+void find_minmax(const vector<double>& numbers, double& min, double& max)
+
+
+
+
+{
+
+min = numbers[0];
+max = numbers[0];
+for (double number : numbers)
+{
+if (number < min)
+{
+min = number;
+}
+if (number > max)
+{
+max = number;
+}
+}
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "histogram.h"
+
+#include <cassert>
+
+int
+main() {
+test_positive();
+test_negative();
+test_same();
+test_onenumber();
+test_empty();
+}
